160e924e765354c641ed9cd19cb6167ae120e4c4
ivfalcon <ivanfloal@gmail.com>
2022-05-19 17:31:29 +0200

    feat(map): Add return navigation

--- map/app.js
+++ map/app.js
@@ -2,20 +2,30 @@ import * as province from './assets/svg_codes.js';
 
 const communitiesEl = document.querySelector('#communities_container');
 const provinceEl = document.querySelector('#province_container');
+const backBtn = document.querySelector('button#back');
 
 const openCommunity = (event) => {
     if (event.target.tagName === 'path') {
-        communitiesEl.className = "d-none";
+        communitiesEl.className = 'd-none';
         const idRegExp = new RegExp(event.target.id, 'i');
         const findedProvince = Object.values(province).find((svg) => { 
             return idRegExp.test(svg);
         })
         provinceEl.innerHTML = findedProvince;
-        provinceEl.removeAttribute("class");
+        provinceEl.removeAttribute('class');
+        backBtn.removeAttribute('class');
     }
 }
 
+const backToCommunities = () => {
+    provinceEl.className = 'd-none';
+    backBtn.className = 'd-none';
+    communitiesEl.removeAttribute('class');
+}
+
 communitiesEl.addEventListener('click', openCommunity);
+backBtn.addEventListener('click', backToCommunities);
+
 
 
 
