ca690c22588ebe451273051c0da7b39775850cce
ivfalcon <ivanfloal@gmail.com>
2022-05-25 22:15:20 +0200

    feat: Add visited cities data

--- map/src/city-data.js
+++ map/src/city-data.js
@@ -1,41 +1,47 @@
-function showCityData() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    const container = document.querySelector('.destiny');
-    const fragment = document.createDocumentFragment();
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
-    fetch("https://mapped-backend-kdjbm.ondigitalocean.app/api/travels/read.php", requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            const result = {};
-            data.body.forEach((travel) => {
-                const currentCity = travel.city_id;
-                result[currentCity] ??= 0;
-                result[currentCity] += 1;
-            })
-            const cityRanking = Object.entries(result).sort((a,b) => b[1] - a[1]);
-            console.log(cityRanking);
-            return cityRanking;
-        }).then(data => {
-            data.forEach(id => {
-                fetch(`https://mapped-backend-kdjbm.ondigitalocean.app/api/cities/read_by_id.php?city_id=${id}`, requestOptions)
-                .then(response => response.json())
-                .then(result => {
-                    console.log(result);
-                    const itemList = document.createElement('li');
-                    itemList.textContent = result.name;
-                    fragment.appendChild(itemList);
-                    console.log(fragment);
+const container = document.querySelector('.destiny');
+const fragment = document.createDocumentFragment();
+
+
+function orderCityVisits() {
+    return new Promise((resolve, reject) => {
+        fetch("https://mapped-backend-kdjbm.ondigitalocean.app/api/travels/read.php", requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                const result = {};
+                data.body.forEach((travel) => {
+                    const currentCity = travel.city_id;
+                    result[currentCity] ??= 0;
+                    result[currentCity] += 1;
                 })
-                .catch(error => console.log('error', error));
+                const cityRanking = Object.entries(result).sort((a, b) => b[1] - a[1]);
+                resolve(cityRanking);
             })
-        }).then(() => {
-            console.log(fragment);
-            container.appendChild(fragment);
+    })
+}
+
+function showCityData() {
+    orderCityVisits()
+        .then(cityArray => {
+            const cityResult = cityArray.map(async cityData => {
+                const cities = await fetch(`https://mapped-backend-kdjbm.ondigitalocean.app/api/cities/read_by_id.php?city_id=${cityData[0]}`, requestOptions)
+                const parsedCities = await cities.json();
+
+                return parsedCities;
+            })
+            Promise.all(cityResult).then(data => {
+                data.forEach((city, index) => {
+                    const itemList = document.createElement('li');
+                    itemList.textContent = `${city.name} - ${cityArray[index][1]}`;
+                    fragment.appendChild(itemList);
+                })
+                container.appendChild(fragment);
+            });
         })
-        .catch(error => console.log('error', error));
 }
 
 export { showCityData };
